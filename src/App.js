1c23372f167fbc0fde91d8b1201e5e2bb3ddd6b3
github-marynapoliarush <merienf8118@gmail.com>
2023-07-23 19:46:34 +0300

    small size styles edits

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './styles.scss';
 import Footer from './shared/Footer/Footer';
 import Header from './shared/Header/Header';
 import Button from './shared/Button/Button';
-import { IconButton } from 'shared/IconButton/IconButton';
 
 const App = () => {
   return (
@@ -13,7 +12,6 @@ const App = () => {
           <h1>RESTio</h1>
           <div className="centered">
             <Button>Start</Button>
-            <IconButton size={33} Svg={BiAddToQueue} disabled={true} mode={'outlined'} />
           </div>
         </div>
       </main>
