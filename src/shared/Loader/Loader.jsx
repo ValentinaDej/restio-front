76c20e6426b5e327ec8388d845c1db3fdd26905f
Khrystyna Krasovska <121851618+KrisKrasovska@users.noreply.github.com>
2023-07-23 15:40:52 +0000

    Add component Loader

---
+++ src/shared/Loader/Loader.jsx
@@ -0,0 +1,29 @@
+import { RotatingLines } from 'react-loader-spinner';
+import PropTypes from 'prop-types';
+import classes from './Loader.module.scss';
+
+const sizeValues = {
+  sm: '54',
+  md: '80',
+  lg: '96',
+};
+
+const Loader = ({ size }) => {
+  return (
+    <div className={`${classes.loader}`}>
+      <RotatingLines
+        strokeColor="#ea6a12"
+        strokeWidth="5"
+        width={sizeValues[size]}
+        animationDuration="0.75"
+        visible={true}
+      />
+    </div>
+  );
+};
+
+Loader.propTypes = {
+  size: PropTypes.oneOf(['sm', 'md', 'lg']),
+};
+
+export default Loader;
