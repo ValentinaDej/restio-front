1f18d3f497bb7efaf603367bacfdf4b1e04b63b8
Khrystyna Krasovska <121851618+KrisKrasovska@users.noreply.github.com>
2023-07-25 14:34:51 +0000

    add storybook for component CardSkeleton

--- src/shared/Skeletons/CardSkeleton/CardSkeleton.jsx
+++ src/shared/Skeletons/CardSkeleton/CardSkeleton.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import css from '../../Card/Card.module.scss';
+import styles from './CardSkeleton.module.scss';
 
 const variant = {
   order: 'order',
@@ -22,24 +23,22 @@ const CardSkeleton = ({ mode = variant.order, ...props }) => {
           <Skeleton width={100} height={20} />
 
           {mode === variant.cart && (
-            <div className={css['card__icon-wrapper']}>
+            <div className={css['card__icon-wrapper']} style={{ marginRight: 0 }}>
               <Skeleton width={30} height={20} />
             </div>
           )}
         </div>
 
-        <div className={css['card__flex-container']}>
+        <div className={css['card__flex-container']} style={{ justifyContent: 'space-between' }}>
           {mode === variant.cart && <Skeleton width={30} height={20} />}
           {mode === variant.waiter && <Skeleton width={30} height={20} />}
           {mode === variant.cook && (
             <>
               <Skeleton width={30} height={20} />
-              <Skeleton width={30} height={20} />
             </>
           )}
           {mode === variant.order && (
             <>
-              <Skeleton width={30} height={20} />
               <Skeleton width={30} height={20} />
             </>
           )}
@@ -49,7 +48,11 @@ const CardSkeleton = ({ mode = variant.order, ...props }) => {
           {mode === variant.order && <Skeleton width={30} height={20} />}
         </div>
       </div>
-      {mode === variant.waiter && <Skeleton width={30} height={20} />}
+      {mode === variant.waiter && (
+        <div className={`${styles.cardSkeleton__waiter}`}>
+          <Skeleton circle width={30} height={30} />
+        </div>
+      )}
     </div>
   );
 };
--- src/shared/Skeletons/CardSkeleton/CardSkeleton.stories.jsx
+++ src/shared/Skeletons/CardSkeleton/CardSkeleton.stories.jsx
@@ -20,3 +20,24 @@ const meta = {
 export default meta;
 const Template = (args) => <CardSkeleton {...args} />;
 export const Default = Template.bind({});
+Default.args = {
+  mode: 'order',
+};
+
+export const Cart = {
+  args: {
+    mode: 'cart',
+  },
+};
+
+export const Waiter = {
+  args: {
+    mode: 'waiter',
+  },
+};
+
+export const Cook = {
+  args: {
+    mode: 'cook',
+  },
+};
