4a35aad2bfee21f3c47cdb90acceeb5bac3e699f
MaistrukHUB <vladyslavmaistruk07@gmail.com>
2023-07-25 11:11:27 +0300

    deleted comments

--- src/shared/Status/Status.stories.jsx
+++ src/shared/Status/Status.stories.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import Status from './Status'; // Replace './Status' with the correct path to your component
+import Status from './Status';
 import '../../styles.scss';
 
 export default {
-  title: 'Shared/Status', // The title to display in Storybook's sidebar
-  component: Status, // The React component that you want to showcase
-  tags: ['autodocs'], // Any tags you want to add (optional)
+  title: 'Shared/Status',
+  component: Status,
+  tags: ['autodocs'],
   argTypes: {
-    // Define your component's props and their types here
-    // Example: (replace with the actual prop types of your component)
     status: {
       type: 'string',
       description: 'The status of the item',
@@ -17,31 +15,22 @@ export default {
         type: 'radio',
       },
     },
-    // Add more props as needed...
   },
 };
 
-// Create a template function that renders your component with the provided args
-const Template = (args) => <Status {...args} />; // Replace 'Status' with your component name
+const Template = (args) => <Status {...args} />;
 
-// Create different stories to showcase different variations of your component
-// You can create as many stories as you want
 export const ActiveStatus = Template.bind({});
 ActiveStatus.args = {
-  status: 'active', // Provide the props specific to this story
+  status: 'active',
 };
 
 export const InactiveStatus = Template.bind({});
 InactiveStatus.args = {
-  status: 'inactive', // Provide the props specific to this story
+  status: 'inactive',
 };
 
-// Add more stories as needed...
-
-// You can also create a "Default" story to showcase your component with default props
 export const Default = Template.bind({});
 Default.args = {
-  // Default props for your component, if applicable
-  // Example: (replace with the default props of your component)
   status: 'active',
 };
