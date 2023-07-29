738a8db243173d8b2483f9c5e92aa9e669754fbe
Khrystyna Krasovska <121851618+KrisKrasovska@users.noreply.github.com>
2023-07-29 16:49:33 +0000

    change styles HomePage

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import Footer from './shared/Footer/Footer';
 import Header from './shared/Header/Header';
 import Button from './shared/Button/Button';
 import { Toaster } from 'react-hot-toast';
+import HomePage from 'pages/HomePage/HomePage';
 
 const App = () => {
   return (
-    <>
-      <div>
-        <Toaster />
-      </div>
-      <Header role="customer" />
-      <main className="main">
-        <div className="main__container">
-          <h1>RESTio</h1>
-          <div className="centered">
-            <Button>Start</Button>
-          </div>
-        </div>
-      </main>
-      <Footer />
-    </>
+    // <>
+    //   <div>
+    //     <Toaster />
+    //   </div>
+    //   <Header role="customer" />
+    //   <main className="main">
+    //     <div className="main__container">
+    //       <h1>RESTio</h1>
+    //       <div className="centered">
+    //         <Button>Start</Button>
+    //       </div>
+    //     </div>
+    //   </main>
+    //   <Footer />
+    // </>
+    <HomePage />
   );
 };
 
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,24 +1,28 @@
 import { NavLink } from 'react-router-dom';
 import styles from './HomePage.module.scss';
+import logo from '../../img/RESTio.svg';
 
 const HomePage = () => {
   return (
     <div className={`${styles.background}`}>
       <main>
-        <div>
-          {/* <img className="image" src={logo} alt="logo" /> */}
-          <h1 className={`${styles.title}`}>Welcome to Restio!</h1>
-          <p className={`${styles.description}`}>
-            We're thrilled to have you on board, and we know you'll take our restaurant to new
-            heights. Let's make each day a delectable delight together! Bon appétit, and let's make
-            some kitchen magic happen!
-          </p>
-          <div className={`${styles.buttons__container}`}>
-            <NavLink className={`${styles.link}`}>Log in</NavLink>
+        <div className="main__container">
+          <div className={`${styles.background__content}`}>
+            <img className={`${styles.img__logo}`} src={logo} alt="logo" width={100} height={100} />
+            <h1 className={`${styles.title}`}>Welcome to Restio!</h1>
+            <p className={`${styles.description}`}>
+              We're thrilled to have you on board, and we know you'll take our restaurant to new
+              heights. Let's make each day a delectable delight together! Bon appétit, and let's
+              make some kitchen magic happen!
+            </p>
+            <div className={`${styles.buttons__container}`}>
+              <NavLink className={`${styles.link}`}>Log in</NavLink>
+            </div>
           </div>
         </div>
       </main>
     </div>
   );
 };
+
 export default HomePage;
