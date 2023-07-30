a9e3d531c2d7dd1e651c0b490f509670c557db81
Khrystyna Krasovska <121851618+KrisKrasovska@users.noreply.github.com>
2023-07-30 12:20:28 +0000

    add Logo component

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,23 +1,23 @@
 import { NavLink } from 'react-router-dom';
 import styles from './HomePage.module.scss';
-import logo from '../../img/RESTio.svg';
 import Title from '../../shared/Title/Title';
+import Text from 'shared/Text/Text';
+import Logo from 'shared/Logo/Logo';
 
 const HomePage = () => {
   return (
     <div className={`${styles.background}`}>
       <main>
         <div className="main__container">
-          <img className={`${styles.img__logo}`} src={logo} alt="logo" width={80} height={80} />
-          <Title mode="h1" color="var(--color-dark-blue)" classname={`${styles.title}`}>
+          <Logo size="lg" classname={`${styles.img__logo}`} />
+          <Title mode="h1" classname={`${styles.title}`}>
             Welcome to Restio!
           </Title>
-          {/* <h1 className={`${styles.title}`}>Welcome to Restio!</h1> */}
-          <p className={`${styles.description}`}>
+          <Text mode="p" classname={`${styles.description}`}>
             We're thrilled to have you on board, and we know you'll take our restaurant to new
             heights. Let's make each day a delectable delight together! Bon appétit, and let's make
             some kitchen magic happen!
-          </p>
+          </Text>
           <div className={`${styles.buttons__container}`}>
             <NavLink className={`${styles.link}`}>Log in</NavLink>
           </div>
