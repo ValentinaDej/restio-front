364d33173f23c8028faaf77e43602004cafbf8c0
Mike Tanchuk <volkermischa@gmail.com>
2023-08-02 15:12:36 +0300

    Implemented minimum working Employee Page with fetching and displaying data. Added minimum working edit empl page.

--- src/shared/EmployeeCard/EmployeeCard.jsx
+++ src/shared/EmployeeCard/EmployeeCard.jsx
@@ -2,18 +2,34 @@ import PropTypes from 'prop-types';
 import classes from './EmployeeCard.module.scss';
 import { BsTrash } from 'react-icons/bs';
 import { BiEditAlt } from 'react-icons/bi';
+import { useNavigate } from 'react-router-dom';
 
 const EmployeeCard = ({ children, onClick, mode = 'primary', size, ...props }) => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`/admin/addPersonnel/${props.data._id}`);
+  };
   return (
     <>
       <div className={`${classes.card} ${classes[`${mode}`]} ${classes[`${size}`]}`}>
         <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5pbc3H9yYgdymFpJ5frN32l13IYwYSocmDA&usqp=CAU"
+          alt={`Employee ${props.data.name}`}
+          src={props.data.picture}
           className={classes.card_image}
         />
         {/* Insert 'DELETE BUTTON" and "EDIT BUTTON" component instead of react-icons, but keep a class '.trash' and '.edit' and size={'1.2rem'}  */}
-        <BsTrash size={'1.2rem'} className={`${classes.trash} ${classes[`${size}`]}`}></BsTrash>
-        <BiEditAlt size={'1.2rem'} className={`${classes.edit} ${classes[`${size}`]}`}></BiEditAlt>
+        <BsTrash
+          onClick={() => {
+            console.log(props.data);
+          }}
+          size={'1.6rem'}
+          className={`${classes.trash} ${classes[`${size}`]}`}
+        ></BsTrash>
+        <BiEditAlt
+          onClick={handleClick}
+          size={'1.6rem'}
+          className={`${classes.edit} ${classes[`${size}`]}`}
+        ></BiEditAlt>
         {/* End of buttons block  */}
         <div className={classes.employee_text}>
           <p className={classes.employee_name}>{props.data.name}</p>
