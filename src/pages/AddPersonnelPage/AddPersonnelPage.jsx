620c3951adb9a071bc86dbfc8ab8a98c4d20f2f1
Mike Tanchuk <volkermischa@gmail.com>
2023-08-02 23:00:25 +0300

    Data fetching rewritten using react query

--- src/pages/AddPersonnelPage/AddPersonnelPage.jsx
+++ src/pages/AddPersonnelPage/AddPersonnelPage.jsx
@@ -1,60 +1,66 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import axios from 'axios';
 import EmployeeForm from '../../shared/EmployeeForm/EmployeeForm';
-import PropTypes from 'prop-types';
 import { useParams } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import Loader from '../../shared/Loader/Loader';
+import styles from './AddPersonnelPage.module.scss';
 
 const AddPersonnelPage = () => {
-  const [data, setData] = useState(null); // State to hold the fetched data
-
   const { personId } = useParams();
 
-  const handleSubmit = (formData) => {
-    console.log(formData);
+  async function fetchData(personId) {
+    try {
+      const response = await axios.get(`http://localhost:3001/personnel/${personId}`);
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  const { data, isLoading } = useQuery(['new_personnel', personId], () => fetchData(personId));
+
+  const handleSubmit = (data) => {
+    console.log(data);
   };
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        // Replace 'your-api-endpoint' with the actual endpoint to fetch the data
-        const response = await axios.get(`http://localhost:3001/personnel/${personId}`);
-        setData(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    }
+  console.log(data);
 
-    fetchData();
-  }, []);
+  if (isLoading) {
+    return (
+      <main className={styles.loadingWrapper}>
+        <Loader size={'lg'} />
+      </main>
+    );
+  }
 
   // Check if data is null before accessing its properties
-  const firstName = data?.name?.substring(0, data.name.indexOf(' ')) || '';
-  const lastName = data?.name?.substring(data.name.indexOf(' ') + 1) || '';
+  const firstName = data.name.substring(0, data.name.indexOf(' ')) || '';
+  const lastName = data.name.substring(data.name.indexOf(' ') + 1) || '';
 
   return (
     <div>
-      <main>
+      <main className={styles.addPersonnelContainer}>
         <h1>Add Personnel</h1>
-        {data !== null ? ( // Render the form only if data is not null
-          <EmployeeForm
-            onSubmit={handleSubmit}
-            size={'md'}
-            initialState={{
-              firstName,
-              lastName,
-              email: data.email,
-              password: '',
-              role: data.role,
-              gender: data.gender,
-              phone: data.phone,
-              address: data.address,
-              image: data.picture,
-            }}
-          />
-        ) : (
-          <p>Loading data...</p>
-        )}
+        <EmployeeForm
+          onSubmit={handleSubmit}
+          size={'md'}
+          initialState={{
+            firstName,
+            lastName,
+            email: data.email,
+            password: '',
+            role: data.role,
+            gender: data.gender,
+            phone: data.phone,
+            address: data.address,
+            image: data.picture,
+          }}
+        />
       </main>
+      <ReactQueryDevtools initialIsOpen />
     </div>
   );
 };
--- src/pages/EmployeePage/EmployeePage.jsx
+++ src/pages/EmployeePage/EmployeePage.jsx
@@ -4,30 +4,22 @@ import EmployeeCard from '../../shared/EmployeeCard/EmployeeCard';
 import axios from 'axios';
 import Title from '../../shared/Title/Title';
 import EmptyCard from '../../shared/EmptyCard/EmptyCard';
+import { useQuery } from 'react-query';
 
 const EmployeePage = ({ restaurant_id }) => {
-  // State to store the fetched data
-  const [personnelData, setPersonnelData] = useState([]);
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3001/personnel/restaurant/${restaurant_id}`
+      );
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
-  // useEffect hook to fetch data when the component is mounted
-  useEffect(() => {
-    // Function to fetch data from the backend server
-    const fetchData = async () => {
-      try {
-        // Replace 'YOUR_BACKEND_ENDPOINT' with your actual backend API endpoint
-        const response = await axios.get(
-          `http://localhost:3001/personnel/restaurant/${restaurant_id}`
-        );
-
-        console.log(response.data);
-
-        setPersonnelData(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-    fetchData(); // Call the data fetching function
-  }, []); // Empty dependency array ensures this effect runs only once, on component mount
+  const { data } = useQuery('personnel', fetchData);
 
   return (
     <>
@@ -35,12 +27,12 @@ const EmployeePage = ({ restaurant_id }) => {
         <div className={styles['personnel-container']}>
           <Title>Personnel</Title>
           <ul className={`${styles.menu_wrapper}`}>
-            <li className={styles.card_wrapper}>
+            <li key={`empty`} className={styles.card_wrapper}>
               <EmptyCard text={`employee`} mode={`outlined`}></EmptyCard>
             </li>
-            {personnelData.map((item) => {
+            {data?.map((item) => {
               return (
-                <li className={styles.card_wrapper} key={item.id}>
+                <li key={item.id} className={styles.card_wrapper}>
                   <EmployeeCard data={item} mode={'outlined'}></EmployeeCard>
                 </li>
               );
