95d9c8b15bf46de42ba41f2547e419f755e3e66a
github-marynapoliarush <merienf8118@gmail.com>
2023-08-03 12:34:27 +0300

    backend connected

--- src/pages/DishPage/DishPage.jsx
+++ src/pages/DishPage/DishPage.jsx
@@ -1,44 +1,55 @@
-import { NavLink } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { NavLink, useParams } from 'react-router-dom';
+import axios from 'axios';
 import classes from './DishPage.module.scss';
 import Title from 'shared/Title/Title';
 import Text from 'shared/Text/Text';
 import QuantityButton from 'shared/QuantityButton/QuantityButton';
 import DishCard from 'shared/DishCard/DishCard';
 import { IoReturnDownBackOutline } from 'react-icons/io5';
-import { FaWeightHanging } from 'react-icons/fa';
 
 const DishPage = (props) => {
+  const [dish, setDish] = useState([]);
+  const params = useParams().dishId;
   const isTrue = true;
+  const getDishById = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3001/dishes/${params}`);
+      console.log(response.data);
+      setDish(response.data);
+      return response.data;
+    } catch (err) {
+      console.error(err.toJSON());
+    }
+  };
+  useEffect(() => {
+    getDishById();
+  }, []);
   return (
-    <div className={classes.dish}>
+    <main className={classes.dish}>
       <NavLink to="/customer/:restId/:tableId" className={classes.back}>
         <IoReturnDownBackOutline></IoReturnDownBackOutline>
         <span>Back to Menu</span>
       </NavLink>
       <div className={classes.fullDish}>
         <div className={classes.dishInfoWarapper}>
-          <img
-            className={classes.dishImage}
-            src={
-              'https://templates.iqonic.design/aprycot/html/dashboard/dist/assets/images/layouts/dish-detail/01.png'
-            }
-          />
+          <img className={classes.dishImage} src={dish.picture} />
           <div className={classes.dishText}>
             <Title mode="h2" classname={classes.dishTitle}>
-              Beef burger
+              {dish.name}
             </Title>
             <div className={`${classes.price_wrapper} ${classes.box} `}>
               <div>
                 <Text mode="p" classname={classes.subtitle}>
                   Price
                 </Text>
-                <span>$ 8</span>
+                <span>${dish.price}</span>
               </div>
               <div>
                 <Text mode="p" classname={classes.subtitle}>
                   Weight
                 </Text>
-                <span>230 g</span>
+                <span>{dish.portionWeight} g</span>
               </div>
               <QuantityButton classname={classes.quantity_buttons}></QuantityButton>
             </div>
@@ -47,12 +58,9 @@ const DishPage = (props) => {
                 Ingredients
               </Text>
               <ul className={classes.ingr_item}>
-                <li>Beef</li>
-                <li>Ketchup</li>
-                <li>Onion</li>
-                <li>Salad</li>
-                <li>Onion</li>
-                <li>Salad</li>
+                {dish.ingredients?.map((item) => {
+                  return <li key={item._id}>{item.name}</li>;
+                })}
               </ul>
             </div>
             <div className={`${classes.spicy_wrapper} ${classes.box} `}>
@@ -61,8 +69,24 @@ const DishPage = (props) => {
                   Vegeterian
                 </Text>
                 <ul>
-                  <li className={`${classes.tab} ${classes[`${isTrue}`]} `}>Yes</li>
-                  <li className={`${classes.tab} ${classes[`${false}`]} `}>No</li>
+                  <li
+                    className={
+                      dish.vegetarian
+                        ? `${classes.tab} ${classes[`${true}`]} `
+                        : `${classes.tab} ${classes[`${false}`]} `
+                    }
+                  >
+                    Yes
+                  </li>
+                  <li
+                    className={
+                      dish.vegetarian
+                        ? `${classes.tab} ${classes[`${false}`]} `
+                        : `${classes.tab} ${classes[`${true}`]} `
+                    }
+                  >
+                    No
+                  </li>
                 </ul>
               </div>
               <div className={classes.spicy_item}>
@@ -70,8 +94,24 @@ const DishPage = (props) => {
                   Pescatarian
                 </Text>
                 <ul>
-                  <li className={`${classes.tab} ${classes[`${isTrue}`]} `}>Yes</li>
-                  <li className={`${classes.tab} ${classes[`${false}`]} `}>No</li>
+                  <li
+                    className={
+                      dish.pescatarian
+                        ? `${classes.tab} ${classes[`${true}`]} `
+                        : `${classes.tab} ${classes[`${false}`]} `
+                    }
+                  >
+                    Yes
+                  </li>
+                  <li
+                    className={
+                      dish.pescatarian
+                        ? `${classes.tab} ${classes[`${false}`]} `
+                        : `${classes.tab} ${classes[`${true}`]} `
+                    }
+                  >
+                    No
+                  </li>
                 </ul>
               </div>
               <div className={classes.spicy_item}>
@@ -79,8 +119,24 @@ const DishPage = (props) => {
                   Spicy
                 </Text>
                 <ul>
-                  <li className={`${classes.tab} ${classes[`${false}`]} `}>Yes</li>
-                  <li className={`${classes.tab} ${classes[`${isTrue}`]} `}>No</li>
+                  <li
+                    className={
+                      dish.spicy
+                        ? `${classes.tab} ${classes[`${true}`]} `
+                        : `${classes.tab} ${classes[`${false}`]} `
+                    }
+                  >
+                    Yes
+                  </li>
+                  <li
+                    className={
+                      dish.spicy
+                        ? `${classes.tab} ${classes[`${false}`]} `
+                        : `${classes.tab} ${classes[`${true}`]} `
+                    }
+                  >
+                    No
+                  </li>
                 </ul>
               </div>
             </div>
@@ -101,7 +157,7 @@ const DishPage = (props) => {
           </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 
