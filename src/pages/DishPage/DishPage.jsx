6005526451cd373858b57cfdc5ac2ae07b965f28
github-marynapoliarush <merienf8118@gmail.com>
2023-08-03 13:45:24 +0300

    style updated

--- src/pages/DishPage/DishPage.jsx
+++ src/pages/DishPage/DishPage.jsx
@@ -10,13 +10,18 @@ import { IoReturnDownBackOutline } from 'react-icons/io5';
 
 const DishPage = (props) => {
   const [dish, setDish] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const params = useParams().dishId;
   const isTrue = true;
   const getDishById = async () => {
     try {
+      setIsLoading(false);
       const response = await axios.get(`http://localhost:3001/dishes/${params}`);
       console.log(response.data);
       setDish(response.data);
+      setTimeout(() => {
+        setIsLoading(true);
+      }, 2000);
       return response.data;
     } catch (err) {
       console.error(err.toJSON());
@@ -26,138 +31,146 @@ const DishPage = (props) => {
     getDishById();
   }, []);
   return (
-    <main className={classes.dish}>
-      <NavLink to="/customer/:restId/:tableId" className={classes.back}>
-        <IoReturnDownBackOutline></IoReturnDownBackOutline>
-        <span>Back to Menu</span>
-      </NavLink>
-      <div className={classes.fullDish}>
-        <div className={classes.dishInfoWarapper}>
-          <img className={classes.dishImage} src={dish.picture} />
-          <div className={classes.dishText}>
-            <Title mode="h2" classname={classes.dishTitle}>
-              {dish.name}
-            </Title>
-            <div className={`${classes.price_wrapper} ${classes.box} `}>
-              <div>
-                <Text mode="p" classname={classes.subtitle}>
-                  Price
-                </Text>
-                <span>${dish.price}</span>
-              </div>
-              <div>
-                <Text mode="p" classname={classes.subtitle}>
-                  Weight
-                </Text>
-                <span>{dish.portionWeight} g</span>
-              </div>
-              <QuantityButton classname={classes.quantity_buttons}></QuantityButton>
-            </div>
-            <div className={classes.box}>
-              <Text mode="p" classname={classes.subtitle}>
-                Ingredients
-              </Text>
-              <ul className={classes.ingr_item}>
-                {dish.ingredients?.map((item) => {
-                  return <li key={item._id}>{item.name}</li>;
-                })}
-              </ul>
-            </div>
-            <div className={`${classes.spicy_wrapper} ${classes.box} `}>
-              <div className={classes.spicy_item}>
-                <Text mode="p" classname={classes.subtitle}>
-                  Vegeterian
-                </Text>
-                <ul>
-                  <li
-                    className={
-                      dish.vegetarian
-                        ? `${classes.tab} ${classes[`${true}`]} `
-                        : `${classes.tab} ${classes[`${false}`]} `
-                    }
-                  >
-                    Yes
-                  </li>
-                  <li
-                    className={
-                      dish.vegetarian
-                        ? `${classes.tab} ${classes[`${false}`]} `
-                        : `${classes.tab} ${classes[`${true}`]} `
-                    }
-                  >
-                    No
-                  </li>
-                </ul>
-              </div>
-              <div className={classes.spicy_item}>
-                <Text mode="p" classname={classes.subtitle}>
-                  Pescatarian
-                </Text>
-                <ul>
-                  <li
-                    className={
-                      dish.pescatarian
-                        ? `${classes.tab} ${classes[`${true}`]} `
-                        : `${classes.tab} ${classes[`${false}`]} `
-                    }
-                  >
-                    Yes
-                  </li>
-                  <li
-                    className={
-                      dish.pescatarian
-                        ? `${classes.tab} ${classes[`${false}`]} `
-                        : `${classes.tab} ${classes[`${true}`]} `
-                    }
-                  >
-                    No
-                  </li>
-                </ul>
-              </div>
-              <div className={classes.spicy_item}>
-                <Text mode="p" classname={classes.subtitle}>
-                  Spicy
-                </Text>
-                <ul>
-                  <li
-                    className={
-                      dish.spicy
-                        ? `${classes.tab} ${classes[`${true}`]} `
-                        : `${classes.tab} ${classes[`${false}`]} `
-                    }
-                  >
-                    Yes
-                  </li>
-                  <li
-                    className={
-                      dish.spicy
-                        ? `${classes.tab} ${classes[`${false}`]} `
-                        : `${classes.tab} ${classes[`${true}`]} `
-                    }
-                  >
-                    No
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
+    <>
+      {isLoading == false ? (
+        <div className={classes.loader}>
+          <div className={classes.spinner}></div>
         </div>
-      </div>
-      <div>
-        <Title mode="h3">Recommend to try</Title>
-        <div className={classes.recommend_wrapp}>
-          <div className={classes.card_wrapper}>
-            <DishCard src="https://templates.iqonic.design/aprycot/html/dashboard/dist/assets/images/layouts/dish-detail/01.png"></DishCard>
+      ) : (
+        <main className={classes.dish}>
+          <NavLink to="/customer/:restId/:tableId" className={classes.back}>
+            <IoReturnDownBackOutline></IoReturnDownBackOutline>
+            <span>Back to Menu</span>
+          </NavLink>
+          <div className={classes.fullDish}>
+            <div className={classes.dishInfoWarapper}>
+              <img className={classes.dishImage} src={dish.picture} />
+              <div className={classes.dishText}>
+                <Title mode="h2" classname={classes.dishTitle}>
+                  {dish.name}
+                </Title>
+                <div className={`${classes.price_wrapper} ${classes.box} `}>
+                  <div>
+                    <Text mode="p" classname={classes.subtitle}>
+                      Price
+                    </Text>
+                    <span>${dish.price}</span>
+                  </div>
+                  <div>
+                    <Text mode="p" classname={classes.subtitle}>
+                      Weight
+                    </Text>
+                    <span>{dish.portionWeight} g</span>
+                  </div>
+                  <QuantityButton classname={classes.quantity_buttons}></QuantityButton>
+                </div>
+                <div className={classes.box}>
+                  <Text mode="p" classname={classes.subtitle}>
+                    Ingredients
+                  </Text>
+                  <ul className={classes.ingr_item}>
+                    {dish.ingredients?.map((item) => {
+                      return <li key={item._id}>{item.name}</li>;
+                    })}
+                  </ul>
+                </div>
+                <div className={`${classes.spicy_wrapper} ${classes.box} `}>
+                  <div className={classes.spicy_item}>
+                    <Text mode="p" classname={classes.subtitle}>
+                      Vegeterian
+                    </Text>
+                    <ul>
+                      <li
+                        className={
+                          dish.vegetarian
+                            ? `${classes.tab} ${classes[`${true}`]} `
+                            : `${classes.tab} ${classes[`${false}`]} `
+                        }
+                      >
+                        Yes
+                      </li>
+                      <li
+                        className={
+                          dish.vegetarian
+                            ? `${classes.tab} ${classes[`${false}`]} `
+                            : `${classes.tab} ${classes[`${true}`]} `
+                        }
+                      >
+                        No
+                      </li>
+                    </ul>
+                  </div>
+                  <div className={classes.spicy_item}>
+                    <Text mode="p" classname={classes.subtitle}>
+                      Pescatarian
+                    </Text>
+                    <ul>
+                      <li
+                        className={
+                          dish.pescatarian
+                            ? `${classes.tab} ${classes[`${true}`]} `
+                            : `${classes.tab} ${classes[`${false}`]} `
+                        }
+                      >
+                        Yes
+                      </li>
+                      <li
+                        className={
+                          dish.pescatarian
+                            ? `${classes.tab} ${classes[`${false}`]} `
+                            : `${classes.tab} ${classes[`${true}`]} `
+                        }
+                      >
+                        No
+                      </li>
+                    </ul>
+                  </div>
+                  <div className={classes.spicy_item}>
+                    <Text mode="p" classname={classes.subtitle}>
+                      Spicy
+                    </Text>
+                    <ul>
+                      <li
+                        className={
+                          dish.spicy
+                            ? `${classes.tab} ${classes[`${true}`]} `
+                            : `${classes.tab} ${classes[`${false}`]} `
+                        }
+                      >
+                        Yes
+                      </li>
+                      <li
+                        className={
+                          dish.spicy
+                            ? `${classes.tab} ${classes[`${false}`]} `
+                            : `${classes.tab} ${classes[`${true}`]} `
+                        }
+                      >
+                        No
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className={classes.card_wrapper}>
-            <DishCard src="https://templates.iqonic.design/aprycot/html/dashboard/dist/assets/images/layouts/dish-detail/01.png"></DishCard>
+          <div>
+            <Title mode="h3">Recommend to try</Title>
+            <div className={classes.recommend_wrapp}>
+              <div className={classes.card_wrapper}>
+                <DishCard src="https://templates.iqonic.design/aprycot/html/dashboard/dist/assets/images/layouts/dish-detail/01.png"></DishCard>
+              </div>
+              <div className={classes.card_wrapper}>
+                <DishCard src="https://templates.iqonic.design/aprycot/html/dashboard/dist/assets/images/layouts/dish-detail/01.png"></DishCard>
+              </div>
+              <div className={classes.card_wrapper}>
+                <DishCard src="https://templates.iqonic.design/aprycot/html/dashboard/dist/assets/images/layouts/dish-detail/01.png"></DishCard>
+              </div>
+            </div>
           </div>
-          <div className={classes.card_wrapper}>
-            <DishCard src="https://templates.iqonic.design/aprycot/html/dashboard/dist/assets/images/layouts/dish-detail/01.png"></DishCard>
-          </div>
-        </div>
-      </div>
-    </main>
+        </main>
+      )}
+    </>
   );
 };
 
