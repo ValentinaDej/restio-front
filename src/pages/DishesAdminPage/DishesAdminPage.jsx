257d8716aac6d675b5f6f86e9eb8b2fab7a33efa
Kristuwa <kristuwa150289@gmail.com>
2023-08-08 02:30:53 +0300

    add select category on page

--- src/pages/DishesAdminPage/DishesAdminPage.jsx
+++ src/pages/DishesAdminPage/DishesAdminPage.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import AdminPageContainer from 'components/Admin/AdminPageContainer/AdminPageContainer';
 import { useMutation, useQuery } from 'react-query';
 import { toast } from 'react-hot-toast';
-import { deleteDishById, fetchDishesList, getDishes } from 'api/dish';
+import { deleteDishById, getDishes } from 'api/dish';
+import Select from 'shared/Select/Select';
+import { DISH_CATEGORIES } from 'utils/constants';
 
 const DishesAdminPage = () => {
   const { restId } = useParams();
   const navigate = useNavigate();
+  const [category, setCategory] = useState('');
 
-  const { data, isLoading, refetch } = useQuery(
-    'fetchDishesList',
-    async () => fetchDishesList(restId),
-    {
-      onError: (error) => {
-        toast.error(error.message);
-      },
-    }
+  const { isError, isLoading, data } = useQuery(
+    ['dishes', category],
+    async () => await getDishes(restId, category)
   );
 
   const { mutateAsync } = useMutation((dishId) => {
@@ -40,15 +38,29 @@ const DishesAdminPage = () => {
     }
   };
 
+  const handleCategory = (e) => {
+    const categoryChoose = e.target.value;
+    setCategory(categoryChoose);
+  };
+
   return (
     <AdminPageContainer
       title="Dishes list"
       variant="dish"
-      data={data}
+      data={data?.data}
       isLoading={isLoading}
       goToAdd={navigateToAddDish}
       handleDelete={handleDelete}
-    />
+    >
+      <Select id="category" value={category} onChange={handleCategory} size="sm" length="sm">
+        <option value="">All category</option>
+        {DISH_CATEGORIES.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </Select>
+    </AdminPageContainer>
   );
 };
 export default DishesAdminPage;
