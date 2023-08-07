efb7f4a1423332f722935477e08b31cbf199f7d2
ValentinaDej <107748584+ValentinaDej@users.noreply.github.com>
2023-08-07 11:23:43 +0300

    added formatDateTime helper

--- src/helpers/formatNumberWithTwoDecimals.js
+++ src/helpers/formatNumberWithTwoDecimals.js
@@ -3,10 +3,8 @@ export const formatNumberWithTwoDecimals = (value) => {
   const parts = formattedValue.split('.');
 
   if (parts.length === 1) {
-    // Якщо немає десяткової частини, додати два нулі
     return `${formattedValue}.00`;
   } else if (parts[1].length === 1) {
-    // Якщо є лише одна цифра після коми, додати один нуль
     return `${formattedValue}0`;
   } else {
     return formattedValue;
