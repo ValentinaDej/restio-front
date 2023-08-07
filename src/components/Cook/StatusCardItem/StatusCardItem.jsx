44309e44e1235579c41563263227d034b8925444
Kristuwa <kristuwa150289@gmail.com>
2023-08-07 15:49:55 +0300

    add propTypes to StatusCardItem

--- src/components/Cook/StatusCardItem/StatusCardItem.jsx
+++ src/components/Cook/StatusCardItem/StatusCardItem.jsx
@@ -1,6 +1,7 @@
 import Status from 'shared/Status/Status';
 import DishesList from '../DishesList/DishesList';
 import styles from './StatusCardItem.module.scss';
+import PropTypes from 'prop-types';
 
 const StatusCardItem = ({ data, handleChangeStatus, status }) => {
   return (
@@ -9,6 +10,19 @@ const StatusCardItem = ({ data, handleChangeStatus, status }) => {
       {data?.length > 0 && <DishesList dishes={data} handleChangeStatus={handleChangeStatus} />}
     </div>
   );
+};
+
+StatusCardItem.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      dish: PropTypes.object.isRequired,
+      quantity: PropTypes.number.isRequired,
+      status: PropTypes.string.isRequired,
+      orderId: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  handleChangeStatus: PropTypes.func.isRequired,
+  status: PropTypes.string.isRequired,
 };
 
 export default StatusCardItem;
