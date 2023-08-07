74c404c8fc6c00c5bf45cb48f76944a5341eb701
ValentinaDej <107748584+ValentinaDej@users.noreply.github.com>
2023-08-07 10:06:12 +0300

    fixed two decimals for integer and one decimal number

--- src/helpers/formatNumberWithTwoDecimals.js
+++ src/helpers/formatNumberWithTwoDecimals.js
@@ -1,4 +1,14 @@
 export const formatNumberWithTwoDecimals = (value) => {
   const formattedValue = parseFloat(value).toFixed(2);
-  return parseFloat(formattedValue);
+  const parts = formattedValue.split('.');
+
+  if (parts.length === 1) {
+    // Якщо немає десяткової частини, додати два нулі
+    return `${formattedValue}.00`;
+  } else if (parts[1].length === 1) {
+    // Якщо є лише одна цифра після коми, додати один нуль
+    return `${formattedValue}0`;
+  } else {
+    return formattedValue;
+  }
 };
