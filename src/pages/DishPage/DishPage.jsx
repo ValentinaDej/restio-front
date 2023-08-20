4c65a3148cfcca6d7b0cae45a47beab952e0a34c
github-marynapoliarush <merienf8118@gmail.com>
2023-08-20 19:23:34 +0300

    style updates

--- src/pages/DishPage/DishPage.jsx
+++ src/pages/DishPage/DishPage.jsx
@@ -7,7 +7,6 @@ import instance from 'api';
 import Title from 'shared/Title/Title';
 import Text from 'shared/Text/Text';
 import QuantityButton from 'shared/QuantityButton/QuantityButton';
-import DishCard from 'shared/DishCard/DishCard';
 import Button from 'shared/Button/Button';
 import { NavigateButtons } from '../../components/Orders/ui/NavigateButtons/NavigateButtons';
 import Cart from 'components/Cart/Cart';
@@ -20,7 +19,7 @@ import { addProduct, decreaseQuantity, increaseQuantity } from 'store/cart/cartS
 import { getProductFromState } from '../../store/cart/cartSelectors';
 import { toast } from 'react-hot-toast';
 
-const DishPage = (props) => {
+const DishPage = () => {
   const [dishQuantity, setDishQuantity] = useState(0);
   const [recommendedDishes, setRecommendedDishes] = useState([]);
   const dishId = useParams().dishId;
@@ -29,8 +28,6 @@ const DishPage = (props) => {
   const dispatch = useDispatch();
   const storeData = useSelector(getProductFromState);
   const { pathname } = useLocation();
-
-  console.log('DishPage', dishId);
   const {
     isLoading,
     data: dish,
@@ -92,7 +89,6 @@ const DishPage = (props) => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
-  
   if (isLoading) {
     return <Loader size="lg"></Loader>;
   }
@@ -120,7 +116,7 @@ const DishPage = (props) => {
     <>
       <main className={classes.dish}>
         <NavLink to={`/${restId}/tables/${tableId}`} className={classes.back}>
-          <NavigateButtons params={restId}>Back</NavigateButtons>
+          <Button mode="outlined">Back</Button>
         </NavLink>
         <div className={classes.fullDish}>
           <div className={classes.dishInfoWarapper}>
