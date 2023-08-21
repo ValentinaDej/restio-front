bb35ab93bc8b31966db83143f7a86b8697ad924e
Lysak-Anna <hvlysak@gmail.com>
2023-08-21 17:20:15 +0300

    ADD: sidebar for admin

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,16 +1,19 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, useLocation } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
 import { useQuery } from 'react-query';
+import { useMediaQuery } from 'react-responsive';
 import { MdRestaurantMenu, MdTableBar } from 'react-icons/md';
 import { FaMoneyBillTrendUp } from 'react-icons/fa6';
 import { IoPeopleSharp } from 'react-icons/io5';
 import { FiLogOut } from 'react-icons/fi';
 import { GiCook } from 'react-icons/gi';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
 import classes from './Header.module.scss';
-import { ReactComponent as Bell } from '../../assets/icons/desk-bell.svg';
+import { ReactComponent as Bell } from 'assets/icons/desk-bell.svg';
 
 import { Sidebar, OrdersButton } from 'components';
 import { callWaiter } from 'api/table';
@@ -22,17 +25,21 @@ export const Header = ({ role }) => {
   const dispatch = useDispatch();
   const restaurantId = useSelector(getRestaurantId);
   const { pathname } = useLocation();
-
+  const [isOpen, setIsOpen] = useState(false);
   const arrParams = pathname.split('/');
   const restId = arrParams[1];
   const tableId = arrParams[3];
-
+  const isMobile = useMediaQuery({
+    query: '(max-width: 767.98px)',
+  });
   const { data } = useQuery(['restaurant', restId], async () => await getRestaurant(restId), {
     refetchOnWindowFocus: false,
     refetchOnReconnect: false,
     refetchInterval: false,
   });
-
+  const toggleMenuHandler = () => {
+    setIsOpen(!isOpen);
+  };
   const logoutHandler = () => {
     dispatch(logout());
   };
@@ -58,31 +65,75 @@ export const Header = ({ role }) => {
       )}
       {role === 'waiter' && <Sidebar />}
       {role === 'admin' && (
-        <div className={classes.header__wrapper}>
-          <NavLink className={classes.header__link} to={`${restaurantId}/admin/dishes`}>
-            <MdRestaurantMenu className={classes.header__icon} />
-          </NavLink>
-          <NavLink className={classes.header__link} to={`${restaurantId}/admin/personnel`}>
-            <IoPeopleSharp className={classes.header__icon} />
-          </NavLink>
-          <NavLink className={classes.header__link} to={`${restaurantId}/waiter/tables`}>
-            <MdTableBar className={classes.header__icon} />
-          </NavLink>
-          <NavLink className={classes.header__link} to={`${restaurantId}/cook`}>
-            <GiCook className={classes.header__icon} />
-          </NavLink>
-          <NavLink className={classes.header__link} to={`${restaurantId}/admin/statistics`}>
-            <FaMoneyBillTrendUp className={classes.header__icon} />
-          </NavLink>
+        <>
+          {isMobile ? (
+            <>
+              {isOpen && (
+                <div className={classes['mobile-menu']}>
+                  <NavLink className={classes.header__link} to={`${restaurantId}/admin/dishes`}>
+                    <MdRestaurantMenu className={classes.header__icon} />
+                  </NavLink>
+                  <NavLink className={classes.header__link} to={`${restaurantId}/admin/personnel`}>
+                    <IoPeopleSharp className={classes.header__icon} />
+                  </NavLink>
+                  <NavLink className={classes.header__link} to={`${restaurantId}/waiter/tables`}>
+                    <MdTableBar className={classes.header__icon} />
+                  </NavLink>
+                  <NavLink className={classes.header__link} to={`${restaurantId}/cook`}>
+                    <GiCook className={classes.header__icon} />
+                  </NavLink>
+                  <NavLink className={classes.header__link} to={`${restaurantId}/admin/statistics`}>
+                    <FaMoneyBillTrendUp className={classes.header__icon} />
+                  </NavLink>
+                  <button className={classes.header__link} onClick={logoutHandler}>
+                    <FiLogOut className={classes.header__icon} />
+                  </button>
+                </div>
+              )}
+            </>
+          ) : (
+            <div className={classes.header__wrapper}>
+              <NavLink className={classes.header__link} to={`${restaurantId}/admin/dishes`}>
+                <MdRestaurantMenu className={classes.header__icon} />
+              </NavLink>
+              <NavLink className={classes.header__link} to={`${restaurantId}/admin/personnel`}>
+                <IoPeopleSharp className={classes.header__icon} />
+              </NavLink>
+              <NavLink className={classes.header__link} to={`${restaurantId}/waiter/tables`}>
+                <MdTableBar className={classes.header__icon} />
+              </NavLink>
+              <NavLink className={classes.header__link} to={`${restaurantId}/cook`}>
+                <GiCook className={classes.header__icon} />
+              </NavLink>
+              <NavLink className={classes.header__link} to={`${restaurantId}/admin/statistics`}>
+                <FaMoneyBillTrendUp className={classes.header__icon} />
+              </NavLink>
+            </div>
+          )}
           <Sidebar />
-        </div>
+        </>
       )}
       {role !== 'customer' && (
-        <div className={classes.header__wrapper}>
-          <button className={classes.header__link} onClick={logoutHandler}>
-            <FiLogOut className={classes.header__icon} />
-          </button>
-        </div>
+        <>
+          {isMobile && role === 'admin' ? (
+            <button
+              className={`${classes['switch-button']} ${isOpen ? classes.open : ''}`}
+              onClick={toggleMenuHandler}
+            >
+              {isOpen ? (
+                <AiOutlineClose className={classes.header__icon} />
+              ) : (
+                <AiOutlineMenu className={classes.header__icon} />
+              )}
+            </button>
+          ) : (
+            <div className={classes.header__wrapper}>
+              <button className={classes.header__link} onClick={logoutHandler}>
+                <FiLogOut className={classes.header__icon} />
+              </button>
+            </div>
+          )}
+        </>
       )}
       {role === 'customer' && (
         <div className={classes.header__wrapper}>
